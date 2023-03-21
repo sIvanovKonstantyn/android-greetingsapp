d5bd0de3044fd4afcd8a33794502408bf24a4ecc
Kostiantyn_Ivanov <ICKostiantyn.Ivanov@amwell.com>
2023-03-21 10:17:23 +0100

    Add main layout with text view, edit text aria and button

--- app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.greetingsapp;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        okButton = findViewById(R.id.okButton);
+
+        okButton.setOnClickListener(
+                v -> Toast.makeText(
+                        MainActivity.this,
+                        String.format("Hello, %s!", editText.getText().toString()),
+                        Toast.LENGTH_LONG
+                ).show()
+        );
     }
 }
